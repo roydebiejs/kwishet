23b09be7a2fcc7a119e31ea56e794f889e34ef73
roy-stapp1solutions <roy@stapp1.solutions>
2024-02-04 15:08:06 +0100

    Update enter kwis to join kwis

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -35,7 +35,7 @@ export default function Header() {
             to="/join-kwis"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Enter Kwis
+            Join Kwis
           </Link>
         </div>
         <div className="flex lg:hidden">
@@ -65,7 +65,7 @@ export default function Header() {
               to="/join-kwis"
               className="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-                Enter Kwis
+                Join Kwis
             </Link>
             <button
               type="button"
