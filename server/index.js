16a6b607e70159076de447e8387c0f02e8ff9b2d
roy-stapp1solutions <roy@stapp1.solutions>
2024-02-04 14:56:05 +0100

    Ja

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from '../client/node_modules/@types/express';
+import cors from 'cors';
+import bodyParser from '../client/node_modules/@types/body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const host = process.env.HOST || 'http://localhost:3000';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const corsOptions = {
+    origin: host,
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
