264d827b1d53d42495efc93fc6ef42a02b4bac2b
roy-stapp1solutions <roy@stapp1.solutions>
2024-02-05 10:05:54 +0100

    fff

--- client/src/pages/JoinKwis.tsx
+++ client/src/pages/JoinKwis.tsx
@@ -1,6 +1,18 @@
+import { useCallback } from "react";
 import Header from "../components/Header";
 
 export default function JoinKwis() {
+  const proxy = process.env.PROXY || "http://localhost:4000";
+  const join = async () => {
+    await fetch(proxy + "/api/")
+      .then((res) => res.json())
+      .then((res) => {
+        if (res) {
+          console.log(res);
+        }
+      });
+  };
+
   return (
     <>
       <Header />
@@ -13,36 +25,33 @@ export default function JoinKwis() {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
           <div className="bg-white px-6 py-12 shadow-xl sm:rounded-lg sm:px-12 border-2 border-slate-100">
-            <form className="space-y-6" action="#" method="POST">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900 text-left"
-                >
-                  Kwis code
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    maxLength={6}
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 uppercase"
-                  />
-                </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900 text-left"
+              >
+                Kwis code
+              </label>
+              <div className="mt-2">
+                <input
+                  id="kwiscode"
+                  maxLength={6}
+                  name="kwiscode"
+                  type="text"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 uppercase"
+                />
               </div>
+            </div>
 
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Join
-                </button>
-              </div>
-            </form>
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={join}
+              >
+                Join
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- server/index.js
+++ server/index.js
@@ -1,30 +1,43 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
-const host = process.env.HOST || 'http://localhost:3000';
+const host = process.env.HOST || "http://localhost:3000";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
 const corsOptions = {
-    origin: host,
-    credentials: true,
-    optionsSuccessStatus: 200
+  origin: host,
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/api/", (req, res) => {
+  res.send({ data: "Hello from Express!" });
 });
 
 const port = process.env.PORT || 4000;
 
 const server = app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
