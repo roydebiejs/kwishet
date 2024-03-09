bda8d39908af768847aa1fe0222416e489192075
roy-stapp1solutions <roy@stapp1.solutions>
2024-03-09 15:28:47 +0100

    Added docker and removed old react version without vite

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Router from "./components/Router";
 
-
 function App() {
   return (
     <div className="App">
--- react/src/pages/JoinKwis.tsx
+++ react/src/pages/JoinKwis.tsx
@@ -1,4 +1,3 @@
-import { useCallback } from "react";
 import Header from "../components/Header";
 
 export default function JoinKwis() {
